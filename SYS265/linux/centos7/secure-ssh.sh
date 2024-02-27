0cc59933b511774f70eacc113313f98be4c94888
eitan-j <47335082+eitan-j@users.noreply.github.com>
2024-02-26 20:54:19 -0500

    fixed secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -10,12 +10,18 @@ fi
 NEW_USER="$1"
 NEW_HOME=/home/"$NEW_USER"
 NEW_SSH="$NEW_HOME"/.ssh
+NEW_KEYS="$NEW_SSH"/authorized_keys
 
-useradd -m -d "$NEW_HOME"
+useradd -m -d "$NEW_HOME" -s /bin/bash "$NEW_USER"
 mkdir "$NEW_SSH"
-cp
+
+cp "$(find / -path "*/tech-journal/SYS265/linux/public-keys/id_rsa.pub")" "$NEW_KEYS"
+# this isn't good, but we don't know where this is other than where it is in comparison to this script
+# finding where this script is is a more complicated problem
+# see: https://stackoverflow.com/q/59895
+
 chmod 700 "$NEW_SSH"
-chmod 600 "$NEW_SSH"/authorized_keys
+chmod 600 "$NEW_KEYS"
 chown -R "$NEW_USER":"$NEW_USER" "$NEW_SSH"
 
 # https://stackoverflow.com/a/46260620
